3ee2dd7e79401ea59a3acf372e782b33ba62d4ee
sameep-git <sameep.shah@tcu.edu>
2024-11-21 00:38:59 -0600

    added user input, working emulator

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -7,6 +7,7 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::keyboard::Keycode;
 
 const SCALE: u32 = 15;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
@@ -20,8 +21,8 @@ fn main() {
         return;
     }
 
-    let sdl_content = sdl2::init().unwrap();
-    let video_subsystem = sdl_content.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
         .window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
@@ -33,14 +34,14 @@ fn main() {
     canvas.clear();
     canvas.present();
 
-    let mut event_pump = sdl_content.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut chip8 = Emu::new();
 
     let mut rom = File::open(&args[1]).expect("Unable to open file 💀");
     let mut buffer = Vec::new();
 
-    rom.read_to_end(& mut buffer).unwrap();
+    rom.read_to_end(&mut buffer).unwrap();
     chip8.load(&buffer);
 
     'gameloop: loop {
@@ -48,6 +49,16 @@ fn main() {
             match evt {
                 Event::Quit { .. } => {
                     break 'gameloop;
+                },
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
                 },
                 _ => ()
             }
@@ -85,4 +96,26 @@ fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
     }
     // Update the canvas
     canvas.present();
+}
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 =>    Some(0x1),
+        Keycode::Num2 =>    Some(0x2),
+        Keycode::Num3 =>    Some(0x3),
+        Keycode::Num4 =>    Some(0xC),
+        Keycode::Q =>       Some(0x4),
+        Keycode::W =>       Some(0x5),
+        Keycode::E =>       Some(0x6),
+        Keycode::R =>       Some(0xD),
+        Keycode::A =>       Some(0x7),
+        Keycode::S =>       Some(0x8),
+        Keycode::D =>       Some(0x9),
+        Keycode::F =>       Some(0xE),
+        Keycode::Z =>       Some(0xA),
+        Keycode::X =>       Some(0x0),
+        Keycode::C =>       Some(0xB),
+        Keycode::V =>       Some(0xF),
+        _ =>                None
+    }
 }
